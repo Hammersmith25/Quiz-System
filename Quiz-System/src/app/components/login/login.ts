b17478afd182accb3358f7984de28eb3ee51c598
Danil G <danil.goncharov25@gmail.com>
2026-04-13 23:26:49 +0500

    Created Login page

---
+++ Quiz-System/src/app/components/login/login.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin() {
+    const data = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.auth.login(data).subscribe({
+      next: (res: any) => {
+        this.auth.setToken(res.access);
+
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        this.error = 'Invalid credentials';
+      }
+    });
+  }
+}
